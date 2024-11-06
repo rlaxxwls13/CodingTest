f5d8800006eb9c51696c00bc03c0aeb9f2d593f1
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-06 22:38:47 +0900

    [level 1] Title: 모의고사, Time: 10.79 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,27 @@
+def solution(answers):
+    n = len(answers)
+    
+    str1 = "12345"
+    str2 = "21232425"
+    str3 = "3311224455"
+    
+    ans1 = str1 * (n//len(str1)) + str1[:n%len(str1)]
+    ans2 = str2 * (n//len(str2)) + str2[:n%len(str2)]
+    ans3 = str3 * (n//len(str3)) + str3[:n%len(str3)]
+    
+    count = [0 for _ in range(3)]
+    
+    for i in range(n):
+        if  answers[i] == int(ans1[i]):
+            count[0] += 1
+        if  answers[i] == int(ans2[i]):
+            count[1] += 1
+        if  answers[i] == int(ans3[i]):
+            count[2] += 1
+    
+    result = []
+    for i in range(3):
+        if count[i] == max(count):
+            result.append(i+1)
+    return result
+       
