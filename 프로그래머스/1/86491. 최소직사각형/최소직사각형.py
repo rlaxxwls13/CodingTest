7f2825ed6a9dff3346d5739f9d4d8b8df691d58d
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-06 21:56:54 +0900

    [level 1] Title: 최소직사각형, Time: 3.39 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -3,6 +3,7 @@ import sys
 INT_MAX = sys.maxsize
 
 def solution(sizes):
+    """
     max_list = []
     min_list = []
     for i in range(len(sizes)):
@@ -13,4 +14,6 @@ def solution(sizes):
             max_list.append(sizes[i][1])
             min_list.append(sizes[i][0])
     return max(max_list)*max(min_list)
+    """
+    return max(max(x) for x in sizes)*max(min(x) for x in sizes)
             
