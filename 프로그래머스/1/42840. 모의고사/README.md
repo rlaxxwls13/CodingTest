d0f0d9f3785828808026d1218dc75480e3dcdb30
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-06 22:48:34 +0900

    [level 1] Title: 모의고사, Time: 9.65 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 10.79 ms
+메모리: 10.1 MB, 시간: 9.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 22:38:45
+2024년 11월 06일 22:48:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,4 +1,5 @@
 def solution(answers):
+    """
     n = len(answers)
     
     str1 = "12345"
@@ -20,6 +21,21 @@ def solution(answers):
             count[2] += 1
     
     result = []
+    for i in range(3):
+        if count[i] == max(count):
+            result.append(i+1)
+    return result
+    """
+    n = len(answers)
+    pattern = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
+    count = [0 for _ in range(3)]
+    result = []
+    
+    for i in range(n):
+        for j in range(3):
+            if pattern[j][i%len(pattern[j])] == answers[i]:
+                count[j] += 1
+    
     for i in range(3):
         if count[i] == max(count):
             result.append(i+1)
