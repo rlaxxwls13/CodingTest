9453ff62b466d23dcacb119e3867344c2a22d607
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-16 20:33:11 +0900

    [D3] Title: 햄버거 다이어트, Time: 680 ms, Memory: 57,040 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,21 +1,17 @@
 T = int(input())
+def dfs(score, kcal, curr_num):
+    global ans
+    ans = max(ans, score)
+    if curr_num == n:
+        return
+
+    if kcal + tk[curr_num][1] <= L:
+        dfs(score + tk[curr_num][0], kcal + tk[curr_num][1], curr_num + 1)
+    dfs(score, kcal, curr_num + 1)
+
 for test_case in range(1, T + 1):
     n, L = map(int, input().split())
-    taste, kcal = [0], [0]
-    for i in range(n):
-        t, k = map(int, input().split())
-        taste.append(t)
-        kcal.append(k)
-
-    dp = [[-float('inf') for i in range(L + 1)] for _ in range(n+1)]
-    
-    dp[0][0] = 0
-    
-    for i in range(1, n+1):
-        for j in range(L + 1):
-            if j - kcal[i] >= 0:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j - kcal[i]] + taste[i])
-            else:
-                dp[i][j] = dp[i-1][j]
-    
-    print(f"#{test_case} {max(dp[n])}")
+    tk = [list(map(int, input().split())) for _ in range(n)]
+    ans = 0
+    dfs(0, 0, 0)
+    print(f"#{test_case} {ans}")
