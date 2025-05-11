a7b79915f17f47b49f4e58434a13eed539054da4
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-05-12 03:22:55 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 771 ms, Memory: 179,308 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,14 +1,16 @@
 T = int(input())
 
+#최댓값일때 팔아야함
+#맨뒤부터 순회하면서 최댓값을 갱신하고 만나는 값이 현재 최댓값보다 작으면 판다, 더 큰 값이면 그 값으로 최댓값을 갱신함
 for test_case in range(1, T + 1):
-    print(f"#{test_case}", end=" ")
     n = int(input())
-    a = list(map(int, input().split()))
-    maximum = max(a)
-    profit = 0
-    for i in range(n-1):
-        if(a[i] == maximum):
-            maximum = max(a[i+1:n])
-            continue
-        profit += maximum - a[i]
-    print(profit)
+    L = list(map(int, input().split()))
+    curr_max = L[-1]
+    ans = 0
+    for i in range(n - 1, -1, -1):
+        if L[i] < curr_max:
+            ans += curr_max - L[i]
+        else:
+            curr_max = L[i]
+        
+    print(f"#{test_case} {ans}")
