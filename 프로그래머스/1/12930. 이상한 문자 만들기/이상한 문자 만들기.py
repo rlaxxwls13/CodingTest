4e69fa884adb16d28914d7827d5e7cc9c73f4fac
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2026-02-10 14:45:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,16 +1,15 @@
 def solution(s):
     idx = 0
     answer = ''
-    for c in s:
-        if c == ' ':
-            idx = 0
-            
+    for c in s: 
         if idx % 2 == 0:
             answer += c.upper()
         else:
             answer += c.lower()
         
-        if c != ' ':
+        if c == ' ':
+            idx = 0
+        else:
             idx += 1
             
     return answer
