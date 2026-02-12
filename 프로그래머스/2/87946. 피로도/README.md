b1c1636bc3a1dd20ed1cb9f93255509194187c06
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2026-02-12 10:54:50 +0900

    [level 2] Title: 피로도, Time: 44.27 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 54.71 ms
+메모리: 9.04 MB, 시간: 44.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 10일 17:30:11
+2026년 02월 12일 10:54:47
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,19 @@
+visited = [False for _ in range(8)]
 ans = 0
-visited = []
 
 def solution(k, dungeons):
-    global visited
-    visited = [False for _ in range(len(dungeons))]
-    choose(dungeons, k, 0)
+    dfs(k, dungeons, 0)
     return ans
-    
-def choose(dungeons, k, cnt):
+
+def dfs(k, dungeons, cnt):
     global ans
     
     ans = max(ans, cnt)
-    
+        
     for i in range(len(dungeons)):
-        if visited[i] == False and k >= dungeons[i][0]:
+        if not visited[i] and k >= dungeons[i][0]:
             visited[i] = True
-            choose(dungeons, k - dungeons[i][1], cnt + 1)
+            dfs(k-dungeons[i][1], dungeons, cnt+1)
             visited[i] = False
-    return
-    
+            
+        
