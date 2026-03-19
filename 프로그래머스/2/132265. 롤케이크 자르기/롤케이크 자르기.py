8f7a42128f9aa627a893316615bf98a0036a9c2f
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2026-03-19 12:54:08 +0900

    [level 2] Title: 롤케이크 자르기, Time: 526.19 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,7 +1,7 @@
 def solution(topping):
     answer = 0
-    cs = {}
-    yh = {}
+    cs = set()
+    yh = dict()
     
     for item in topping:
         if item in yh:
@@ -10,17 +10,12 @@ def solution(topping):
             yh[item] = 1
             
     for item in topping:
-        if item in cs:
-            cs[item] += 1
-        else:
-            cs[item] = 1
-        
+        cs.add(item)
         yh[item] -= 1
+        
         if yh[item] == 0:
             del yh[item]
-            
         if len(yh) == len(cs):
             answer += 1
-            
         
     return answer
